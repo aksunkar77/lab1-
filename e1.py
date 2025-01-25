7cc304aff240a1eba6c5fb08605c67d6aea76a46
aksunkar77 <a_serekbol@kbtu.kz>
2025-01-25 11:09:58 +0500

    Create e1.py

---
+++ e1.py
@@ -0,0 +1,22 @@
+#python casting 
+#float
+a = float(1)     # x will be 1.0
+b = float(2.8)   # y will be 2.8
+c = float("3")   # z will be 3.0
+d = float("4.2") # w will be 4.2
+print(a, b, c, d)
+print(a+b+c+d)
+
+#integer
+e = int(1)   # x will be 1
+f = int(2.8) # y will be 2
+g = int("3") # z will be 3
+print(e, f, g)
+print(e+f+g)
+
+#string
+h = str("s1") # x will be 's1'
+i = str(2)    # y will be '2'
+j = str(3.0)  # z will be '3.0' 
+print(h, i, j)
+print(h+i+j)

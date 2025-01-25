dbe00ee2c64602a9d348d5e761eb9ef2af5f4ced
aksunkar77 <a_serekbol@kbtu.kz>
2025-01-25 11:48:06 +0500

    Create g.py

---
+++ g.py
@@ -0,0 +1,34 @@
+# python strings:in this file i will practice python strings 
+#string
+print("hello my name is Aksunkar")
+#string variable
+my_age=19
+print("i am " +str( my_age) +"years old")
+#string length
+a = "let's gues how many characters in this sentence"
+print(len(a))
+#loop
+for x in "Aksunkar":
+  print(x)
+  #present example
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+  #slicing strings
+a="aksunkar"
+print(a[0:4]) #it is my nickname
+#modify strings
+a = "My name is Aksunkar"
+print(a.upper())
+#string concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+#creating f string
+age = 19
+txt = f"My name is Aksunkar, I am {age}"
+print(txt)
+''' 
+this is my practice from w3school and i followed 13 topics from githowto and this is examples and exercises from w3school thank for your attenttiom
+'''

9d1351c59048d7b5d6df3178a23862a93e036e0b
aksunkar77 <a_serekbol@kbtu.kz>
2025-01-25 10:33:09 +0500

    Create d.py

---
+++ d.py
@@ -0,0 +1,16 @@
+# python data types 
+#in this file i will pracrice python data types
+a="hello,world"
+b=5
+c=5.5
+d = ["apple", "banana", "cherry"]
+e = ("apple", "banana", "cherry")
+f = range(6)
+g = {"name" : "Aksu", "age" : 19}
+print(a + " -> " + str(type(a)))   
+print(str(b) + " -> " + str(type(b))) 
+print(str(c) + " -> " + str(type(c))) 
+print(str(d) + " -> " + str(type(d)))  
+print(str(e) + " -> " + str(type(e)))  
+print(str(f) + " -> " + str(type(f)))  
+print(str(g) + " -> " + str(type(g))) 
